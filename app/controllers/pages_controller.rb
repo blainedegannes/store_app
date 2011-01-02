3a62e0f152b41e674e424a6757edd106ebed7e9f
Blaine Degannes <blaine@entreelife.com>
2011-01-01 21:12:31 -0500

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def sales
+    @title = "Sales"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+  
+  before (:each) do
+    @base_title = "Entree"
+  end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+          get 'about'
+          response.should have_selector("title", :content => @base_title + " | About")
     end
   end
 
@@ -14,12 +24,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+            get 'contact'
+            response.should have_selector("title", :content => @base_title + " | Contact")
+    end
   end
 
   describe "GET 'sales'" do
     it "should be successful" do
       get 'sales'
       response.should be_success
+    end
+   
+    it "should have the right title" do
+            get 'sales'
+            response.should have_selector("title", :content => @base_title + " | Sales")
     end
   end
 
